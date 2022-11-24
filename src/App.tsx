d9ea5aec7c86fb43c30558b37fa516a45dd7c54c
kurlayusha <tbark.guard@gmail.com>
2022-11-24 20:51:15 +0300

    map is successfuly added to map component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { useState, useEffect } from 'react'
+
 import Header from './components/Header'
 import Info from './components/Info'
 import Map from './components/Map'
 import './App.css'
+
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+} from 'react-leaflet'
 
 // https://geo.ipify.org/api/v2/country,city?apiKey=at_EoVlHExLepBoUY7uq3qXw3ctJKWT2&ipAddress=8.8.8.8
 
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,34 @@
+import 'leaflet/dist/leaflet.css'
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+} from 'react-leaflet'
+
 function Map() {
-  return <div className="map">Map</div>
+  return (
+    <div className="map" style={{ height: '500px', width: '900px' }}>
+      Map
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  )
 }
 
 export default Map
