2cb3ad810077feacf137d0d17eee467faf4d725f
kurlayusha <tbark.guard@gmail.com>
2022-11-24 19:45:27 +0300

    in header component: input and back ground is improt

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,31 @@
-// https://geo.ipify.org/api/v2/country,city?apiKey=at_EoVlHExLepBoUY7uq3qXw3ctJKWT2&ipAddress=8.8.8.8
+import arrow from '../images/icon-arrow.svg'
+import backGround from '../images/pattern-bg.png'
 
 function Header() {
-  return <div className="App">header component</div>
+  return (
+    <div className="App">
+      <section>
+        <div>
+          <img src={backGround} alt="" />
+        </div>
+        <article>
+          <h2>IP Address</h2>
+
+          <form>
+            <input
+              type="text"
+              name="ipaddress"
+              id="ipadress"
+              placeholder="Search for any IP address or domain"
+            />
+            <button type="submit">
+              <img src={arrow} alt="" />
+            </button>
+          </form>
+        </article>
+      </section>
+    </div>
+  )
 }
 
 export default Header
