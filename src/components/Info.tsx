e95c1b6e7c896ffd2827eccd5508cfcb5042664a
kurlayusha <tbark.guard@gmail.com>
2022-11-24 19:54:28 +0300

    hard coded info component and installed react-leaflet

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,5 +1,24 @@
 function Info() {
-  return <div className="info">INFO</div>
+  return (
+    <div className="info">
+      <div>
+        <h2>IP Address</h2>
+        <p>192.212.174.101</p>
+      </div>
+      <div>
+        <h2>LOCATION</h2>
+        <p>Brooklyn, NY 10001</p>
+      </div>
+      <div>
+        <h2>TIMEZONE</h2>
+        <p>UTC -05:00</p>
+      </div>
+      <div>
+        <h2>ISP</h2>
+        <p>SpaceX Starlink</p>
+      </div>
+    </div>
+  )
 }
 
 export default Info
