1342dfc7df9b67858521e9cd6752f84c2aad9842
kurlayusha <tbark.guard@gmail.com>
2022-11-25 17:22:45 +0300

    info css is done

--- src/components/info/Info.tsx
+++ src/components/info/Info.tsx
@@ -8,23 +8,23 @@ function Info({ address }: infoProps) {
   return (
     <>
       {address && (
-        <div className="info">
-          <div>
+        <div className="info--container">
+          <div className="info one">
             <h2>IP Address</h2>
             <p>{address.ip}</p>
           </div>
-          <div>
+          <div className="info two">
             <h2>LOCATION</h2>
             <p>
               {' '}
               {address.location.city}, {address.location.region}
             </p>
           </div>
-          <div>
+          <div className="info three">
             <h2>TIMEZONE</h2>
             <p>UTC {address.location.timezone}</p>
           </div>
-          <div>
+          <div className="info four">
             <h2>ISP</h2>
             <p>{address.isp}</p>
           </div>
